3a29943b2ef471e03540311beb8a14f17090d8b7
Seedy <seedy_seed@hotmail.com>
2025-07-07 01:41:28 +0100

    The compile script can now copy Thrive Messenger's audio files and srv.conf.

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,4 @@
 @echo off
 python versionfile.py
 pyinstaller --windowed --version-file=vdata.txt --upx-dir=C:\UPX tmsg.py
+robocopy . dist\tmsg contact_online.wav login.wav logout.wav send.wav receive.wav srv.conf
